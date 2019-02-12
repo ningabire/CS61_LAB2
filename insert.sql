77ad76092ea3103ebaa6f9f94ae7154c6168b409
ningabire <40244455+ningabire@users.noreply.github.com>
2019-02-11 20:20:24 -0500

    issue and more manuscript data

--- insert.sql
+++ insert.sql
@@ -1,1 +1,20 @@
 INSERT INTO `Manuscript` (`manu_title`,`manu_date`,`manu_pagenb`,`Editor_editor_id`,`Icode_Icode_num`,`Issue_Issue_id`,`manu_status`) VALUES ("vulputate, lacus. Cras","12/07/1818",3,1,1,NULL,9),("gravida. Praesent eu","09/09/1818",6,1,1,NULL,7),("feugiat placerat velit. Quisque","12/02/1919",7,1,1,NULL,4),("Sed nunc est, mollis","10/13/1818",1,1,1,NULL,1),("Quisque","10/04/1818",10,1,1,NULL,10),("Mauris","04/23/1919",6,1,1,NULL,7),("bibendum sed, est.","12/09/1919",4,1,1,NULL,7),("felis.","02/23/1919",6,1,1,NULL,9),("dolor sit amet, consectetuer","11/27/1818",8,1,1,NULL,1),("eget lacus.","08/09/1919",1,1,1,NULL,10);
+
+INSERT INTO `Manuscript` (`manu_title`,`manu_date`,`manu_pagenb`,`Editor_editor_id`,`Icode_Icode_num`,`Issue_Issue_id`,`manu_status`) 
+VALUES('ahdioadjal', "12/07/1818", 13,1, 3, 52, 10), ('barakljop', "12/07/1818", 20,1, 3, 52, 10), ('shiaduajsda', "12/07/1818", 25,1, 3, 51, 10),('oindoada', "01/12/1990", 40,1, 3, 50, 10), ('niajkdnlad', "12/07/1818",5,1,3,54,10), ('ewpmklm', "01/12/1990", 13,1, 3, 56, 10);
+
+/* Issue */
+insert INTO Issue (issue_id, issue_year, issue_periodnum, issue_status, issue_npages, issue_date) 
+VALUES (50, 2015, 1, 'Published', 120, '1000-01-01 00:00:00');
+insert INTO Issue (issue_id, issue_year, issue_periodnum, issue_status, issue_npages, issue_date) 
+VALUES (51, 2015, 1, 'Published', 125, '1000-01-01 00:00:00');
+insert INTO Issue (issue_id, issue_year, issue_periodnum, issue_status, issue_npages, issue_date) 
+VALUES (52, 2015, 3, 'Published', 140, '1000-01-01 00:00:00');
+insert INTO Issue (issue_id, issue_year, issue_periodnum, issue_status, issue_npages, issue_date) 
+VALUES (53, 2017, 2, 'Published', 125, '1000-01-01 00:00:00');
+insert INTO Issue (issue_id, issue_year, issue_periodnum, issue_status, issue_npages, issue_date) 
+VALUES (54, 2017, 4, 'Published', 100, '1000-01-01 00:00:00');
+insert INTO Issue (issue_id, issue_year, issue_periodnum, issue_status, issue_npages, issue_date) 
+VALUES (55, 2019, 1, 'Pending', 120, '1000-01-01 00:00:00');
+insert INTO Issue (issue_id, issue_year, issue_periodnum, issue_status, issue_npages, issue_date) 
+VALUES (56, 2019, 2, 'Pending', 114, '1000-01-01 00:00:00');
